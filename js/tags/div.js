d4a7a6cd9837452b41dcb0295221cf2ae59c7213
Miguel235711 <a01423629@itesm.mx>
2021-03-07 18:23:42 -0600

    support for children div

--- js/tags/div.js
+++ js/tags/div.js
@@ -3,10 +3,9 @@
 import tag from './tag.js'
 import {withChildrenF} from '../../js/transformers/ref.js'
 
-export default (childrenFunctions,{classes,id,text,click}={}) => {
+export default ({classes,id,text,click,childrenFunctions}={},) => {
     let _div = tag('div',{classes:classes,id:id,text:text,click:click})
     return childrenFunctions ? 
-        withChildrenF({elementFunction:_div,childrenFunctions:childrenFunctions}):
-        _div
+        withChildrenF({elementFunction:_div,childrenFunctions:childrenFunctions}):_div
 }
         
