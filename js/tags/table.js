f70e14aa02d96c6aa3ec8ff165ea60415d6e2bb1
Miguel235711 <a01423629@itesm.mx>
2021-03-13 02:05:03 -0600

    table functionality extended

--- js/tags/table.js
+++ js/tags/table.js
@@ -1,13 +1,15 @@
 import tag from './tag.js'
 
 
-let td = ({classes,id,text}={}) => tag('td',{classes:classes,id:id,text:text}) 
+///modify in framework
 
+let td = ({classes,id,text,border}={}) => tag('td',{classes:classes,id:id,text:text,border:border}) 
+let th = ({classes,id,text,border}={}) => tag('th',{classes:classes,id:id,text:text,border:border}) 
 
 ///headerFunction needs to generate the data as []
 ///contentFunction needs to generate the data as [[],[],...]
-export default ({classes,id,headerContentFunction,bodyContentFunction}={}) => () => {
-    let _table = tag('table',{classes:classes,id:id})()//TODO: define tag later!!!
+export default ({classes,id,headerContentFunction,bodyContentFunction,border}={}) => () => {
+    let _table = tag('table',{classes:classes,id:id,border:border})()//TODO: define tag later!!!
 
     //header creation
     let _thead = (()=>{
@@ -17,7 +19,7 @@ export default ({classes,id,headerContentFunction,bodyContentFunction}={}) => ()
         
         console.log('headerContent: ',headerContentFunction())
 
-        headerContentFunction().forEach(content=>_tr.appendChild(td({text:content})()))
+        headerContentFunction().forEach(content=>_tr.appendChild(th({text:content,border:border})()))
         _thead.appendChild(_tr)
         return _thead
     })()
@@ -26,7 +28,7 @@ export default ({classes,id,headerContentFunction,bodyContentFunction}={}) => ()
         let _tbody = tag('tbody')() 
         bodyContentFunction().forEach(row=>{
             let _tr = tag('tr')()
-            row.forEach(content=>_tr.appendChild(td({text:content})()))
+            row.forEach(content=>_tr.appendChild(td({text:content,border:border})()))
             _tbody.appendChild(_tr)
         })
         return _tbody
